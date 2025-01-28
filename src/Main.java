6f97a59d9972dbe09dfc5679ce9271eeaa83b768
Henrique <henrique_krf@outlook.com>
2025-01-28 20:18:34 -0300

    Tipos primitivos

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        byte b = 100;
+        short s = 10000;
+        int i = 100000;
+        long l = 100000L;
+        float f = 10.5f;
+        double d = 20.5;
+        char c = 'A';
+        String str = "Henrique";
+        boolean bool = true;
+    }
+}
