133a519041f9536c63c1e0094a2d2103916ab0a3
Henrique <henrique_krf@outlook.com>
2025-01-29 08:42:03 -0300

    Vetores, if / else e ArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         byte b = 100;
@@ -9,5 +11,36 @@ public class Main {
         char c = 'A';
         String str = "Henrique";
         boolean bool = true;
+
+        int[] colecaoDeInteiros = {1, 2, 3, 4, 5};
+
+        if(bool) {
+            System.out.println("Verdadeiro");
+        } else {
+            System.out.println("Falso");
+        }
+
+        if(str == "Jose") {
+            System.out.println("Verdadeiro");
+        } else if (str == "Rodrigues"){
+            System.out.println("Rodrigues");
+        }else {
+            System.out.println("Falso");
+        }
+
+        System.out.println(colecaoDeInteiros[0]);
+
+        ArrayList<String> nomes = new ArrayList<>();
+
+        nomes.add("Henrique");
+        nomes.add("Jose");
+        nomes.add("Maria");
+        nomes.add("Fulano");
+        System.out.println(nomes.get(0));
+
+        nomes.remove(0);
+
+        System.out.println(nomes.get(0));
+
     }
 }
