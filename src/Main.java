aaeff0c41a8ef7399af6fda76f7b5b99e1b7457b
Henrique <henrique_krf@outlook.com>
2025-01-29 10:16:07 -0300

    Loops e Casting

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         byte b = 100;
         short s = 10000;
-        int i = 100000;
+        /*int i = 100000;*/
         long l = 100000L;
         float f = 10.5f;
         double d = 20.5;
@@ -42,5 +42,25 @@ public class Main {
 
         System.out.println(nomes.get(0));
 
+        for(int i = 0; i < nomes.size(); i++){
+            System.out.println(nomes.get(i));
+        }
+
+        // outra maneira
+
+        for(String nome : nomes) {
+            System.out.println(nome);
+        }
+
+        double resultado = 0.0;
+        int resultadoInt = (int) resultado;
+
+        int meuInt = 10;
+        double meuDouble = meuInt;
+
+        String meuString = "10";
+        int meuInt2 = Integer.parseInt(meuString);
+
+        String minhaString = String.valueOf(meuInt2);
     }
 }
